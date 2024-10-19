253ea5b544d3f5eb78e8eba64d3a226669317641
cr3ativ3cod3r <tkthulasimandiram@gmail.com>
2024-10-20 02:50:09 +0530

    Its who you are underneath

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function decodeClue() {
 }
 
 function treasureCipher(clue, key) {
+    var out = document.getElementById('meow')
     return clue.split('').map(char => {
         if (char.match(/[a-z]/i)) {
             const charCode = char.charCodeAt(0);
             const base = charCode >= 97 ? 97 : 65;
             return String.fromCharCode(((charCode - base + key) % 26 + 26) % 26 + base);
         }
+        out.innerHTML = "Is the output not visible? Hahahahahaha......buhahaha may be try take a look at the files especially the css";
         return char;
+        
     }).join('');
 }
